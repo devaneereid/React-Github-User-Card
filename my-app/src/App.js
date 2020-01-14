e0c6b9aa7144daa4bac6ecfda386e633693bcbb4
Devanee Reid <devaneereid@gmail.com>
2020-01-14 13:01:18 -0700

    receiving data back

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [],
+    userText: []
+  };
+
+  componentDidMount() {
+    axios 
+      .get(`https://api.github.com/users/devaneereid`)
+      .then(res => {
+        this.setState({
+          users: res.data.message
+        });
+        console.log(res)
+      })
+      .catch(err => console.log('Check for User Errors', err));
+
+      axios 
+        .get(`https://api.github.com/users/devaneereid/followers`)
+        .then(res => {
+          this.setState({
+            userText: res.data
+          });
+          console.log(res)
+        })
+        .catch(err => console.log('Check for Follower Errors', err));
+    }
+
+
+  render() {
+    return (
+      <div className="App">
+      
+      </div>
+    );
+  }
 }
 
 export default App;
